90ed88affcb65200d563483597aa1ac7c973eb5a
Marin <Faven375@mail.ru>
2022-04-08 17:39:13 +0300

    mobil version for partners

--- src/templates/PartnersAndClients.tsx
+++ src/templates/PartnersAndClients.tsx
@@ -2,14 +2,20 @@
 import { Carousel } from '@trendyol-js/react-carousel';
 
 import { PartnersAndClientsItem } from '../feature/PartnersAndClientsItem';
+import useWindowDimensions from '../feature/UseWindowDimensions';
 import { Section } from '../layout/Section';
 
-const PartnersAndClients = () => (
-  <Section
-    title="Your title here"
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
-  >
-    <Carousel show={5} slide={2} transition={0.5} swiping={true}  useArrowKeys={false} className="carousel-container">
+
+const PartnersAndClients = () => {
+  const { width } = useWindowDimensions();
+  const showNrSlide = width != null && width <= 950 ? 3 : 5;
+
+  return(
+    <Section
+      title="Your title here"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus malesuada nisi tellus, non imperdiet nisi tempor at."
+    >
+      <Carousel show={showNrSlide} slide={2} transition={0.5} swiping={true}  useArrowKeys={false} className="carousel-container">
         <PartnersAndClientsItem 
           image="/assets/images/our partners/Andy's.svg"
           imageAlt=""
@@ -54,8 +60,9 @@ const PartnersAndClients = () => (
           image="/assets/images/our partners/Vioinox.svg"
           imageAlt=""
         />
-    </Carousel>
-  </Section>
-);
+      </Carousel>
+    </Section>
+  );
+};
 
 export { PartnersAndClients };
