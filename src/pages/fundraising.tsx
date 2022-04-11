c57bd6a0e85a7586b5a544c8c55c1f9bc30f4240
Chizim Cezar <chizimcezar@usa.com>
2022-04-11 13:48:48 +0300

    Add texts

--- src/pages/fundraising.tsx
+++ src/pages/fundraising.tsx
@@ -8,51 +8,53 @@ const Fundraising = () => {
       <div className="max-w-screen-lg mx-auto px-3 mb-28">
         <VerticalFeatureRow
           title="Introduction"
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="Raising finance is an important component of starting and growing a business. After all, having enough funding 
+          to launch a new product or expand into a new market might spell the difference between success and failure. This crucial 
+          responsibility frequently falls on the founder. Even though fundraising is a crucial task, the process can be challenging. 
+          Despite the fact that few founders start out as fundraising specialists, some do eventually get the hang of it. Early 
+          funding sources like as incubators, accelerators, and angel investors, typically start with a pre-seed or seed round. These 
+          rounds can range from EUR 10,000 to EUR 2 million. Companies can use early investment rounds to fund things like market 
+          research and development."
           image="/assets/images/fund-photo1.svg"
           imageAlt="image"
           icon
         />
         <VerticalFeatureRow
           title="Strategy"
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="Different investors have their own set of criteria that we should be aware of before approaching them for 
+          investment. We will not just express your own idea in order to receive investment; we will also comprehend the investor, 
+          their concerns, and what they are looking for. Researching the potential investor in addition to improving your proposal. 
+          Our team will find out if they&#39;ve already invested in a business that&#39;s similar to yours. If this is the case, they 
+          may find it difficult to invest in you as well. Companies then raise higher sums in series A, B, C, and beyond. Family 
+          offices, venture capital, hedge funds, and other investment businesses may be among the investors. Businesses may need to 
+          raise additional capital as they get larger in order to acquire companies, expand into new markets, or develop new products. 
+          Private equity, strategic partners, or an initial public offering are all options for investors at this point."
           image="/assets/images/fund-photo2.svg"
           imageAlt="image"
           reverse
         />
         <VerticalFeatureRow
           title="Proces"
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="We invest time in developing diligence documents for a seed round, an executive summary and a slide deck we can 
+          walk investors through and, potentially, leave behind so VCs can show to other partners. FINHUB is an expert in creating 
+          accurate financial models and business plans that allow everyone to understand how numbers work, not just what numbers are. 
+          We include vision, product, team (location, contact info), traction, market size, and minimum financials (revenue, if any, 
+            and fundraising prior and current).  Generally, we make sure the slide deck is well structured. Graphics, charts, 
+            screenshots are more powerful than lots of words. Consider it a framework around which we will hang a more detailed 
+            version of your story. There is no fixed format or order, but the following parts are usually present. We create the pitch 
+            that matches you, how you present, and how you want to represent your company."
           image="/assets/images/fund-photo3.svg"
           imageAlt="image"
         />
         <VerticalFeatureRow
           title="Activity"
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="There 2 fundraising mechanisms at FINHUB. The Crowdfunding App and the Investment Fund. Place your project on 
+          the platform, as soon as you pass verification, you will be contacted by one of our analysts that will assist you throughout 
+          the entire process. Our team will help you collect and/or create all the necessary documentation to ensure the highest 
+          possible success rate. We will concentrate our efforts to become your ambassador within our own network of private and 
+          institutional investors. The Finhub Capital Fund is constantly evaluating new investment opportunities. We are glad to meet 
+          talented entrepreneurs and create strong, long-term partnerships. Feel free to contact Mr. Artiom Cebanu - Director of 
+          Corporate Affairs & Communications."
           image="/assets/images/fund-photo4.svg"
           imageAlt="image"
           reverse
--- src/pages/product-development.tsx
+++ src/pages/product-development.tsx
@@ -21,39 +21,39 @@ const ProductDevelopment = () => {
           icon
         />
         <VerticalFeatureRow
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="We can move forward with product development once we have the user requirements and functional design. 
+          Software developers, the product manager, and at least one visual designer are normally involved in this stage."
           image="/assets/images/product-photo2.svg"
           imageAlt="image"
+          listItems={[
+            'The visual designer is in charge of translating the functional design into a visual representation, and he or she should be given as much leeway as possible while keeping in mind all of the functionality that is essential when creating a fantastically rich user experience. ',
+            'A complete technical document is provided to all software developers, which offers an in-depth explanation of the functional design from their perspective.',
+          ]}
           reverse
         />
         <VerticalFeatureRow
           title="Design"
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="The design stage’s output is a clear representation of the entire product design process that can be used to 
+          communicate with developers, graphic designers, and content providers. Throughout the whole product life cycle, we conduct 
+          usability testing and quality assurance evaluations. This guarantees that the product is simple to use and that users have 
+          a good time with it. We employ a combination of heuristic evaluation techniques and user testing sessions. Our services 
+          include a lot of support, updates, and product maintenance. We give technical assistance and are constantly adding new 
+          features and functionality to the items we design."
           image="/assets/images/product-photo3.svg"
           imageAlt="image"
           falseElem="team-extension"
         />
         <VerticalFeatureRow
           title="Team Extension"
-          description="A good understanding of the requirements, users, target audience, and prospective market is required for 
-        creating a successful product. We take all of these factors into account and create a well-defined plan that outlines 
-        our approach and strategy for converting your concept into a marketable product. During the first stage, we’ll try to 
-        figure out what we need to know about the product and the company that’s behind it. We accomplish this by conducting 
-        extensive research that provides us with a clear picture of what the product is all about, what it offers users, and what 
-        they require. The research produces a list of Requirement Specifications. The intricacies of the interface, navigation, 
-        and how the information is displayed are identified and defined by functional and visual design."
+          description="If you don’t have specific knowledge in-house, can’t locate the proper personnel locally, or recruiting takes 
+          too long when you’re trying to build new products and grow your business, team extension is the service for you. Finhub can 
+          help you expand your software development team with specialized expertise. We assist businesses in a variety of industries 
+          with augmenting their teams with our professionals to meet the demands of their cutting edge products. We employ young and 
+          motivated IT professionals through a rigorous recruitment process that combines a variety of soft & hard skills assessments. 
+          It allows us to identify and secure top tier candidates only. Learning & Development is an essential part of Finhub’s culture. 
+          Thanks to our dedicated L&D experts, we keep our employees up to date with the latest technology & market trends. We consider 
+          language and communication skills a key factor for personal growth. Offering a comprehensive English course, for business and 
+          IT, we ensure best quality cooperation with all of our partners and customers worldwide."
           image="/assets/images/product-photo4.svg"
           imageAlt="image"
           icon
